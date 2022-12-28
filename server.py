7b5735f225cc8a59a305d9de2334c6e3ffe2bbec
iustina-d <iustina2dulea@gmail.com>
2022-12-28 17:21:19 +0200

    create basic structure

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template
+import os
+
+app = Flask(__name__)
+app.secret_key = "bhafiow vnkahbdsejr/"
+
+
+UPLOAD_FOLDER = (
+    os.getenv("UPLOAD_FOLDER") if "UPLOAD_FOLSER" in os.environ else "images" 
+)
+app.config["UPLOAD_FOLDER"] =UPLOAD_FOLDER
+
+@app.route("/")
+def main():
+    return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
